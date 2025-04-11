59d6388e28198258bc57e08ae2176c47230b789f
RezaBanivakil <rezabaniv@gmail.com>
2025-04-11 22:54:53 +0330

    0.1.3
    implemented without calculating primes list

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,19 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut output = Vec::with_capacity(1);
-    let mut primes = Vec::new();
 
-    let mut num = 2;
     let mut n = n;
     while n != 1 {
-        let prime = primes.iter().filter(|prime| n % *prime == 0).take(1).next();
-        match prime {
-            Some(prime) => {
-                n /= prime;
-                output.push(*prime);
+        let i = (2..=n).filter(|i| n % i == 0).next();
+        match i {
+            Some(i) => {
+                n /= i;
+                output.push(i);
                 continue;
             }
             None => {
-                update_primes(&mut primes, &mut num);
+                panic!("Should not happen");
             }
         }
     }
     output
-}
-
-fn update_primes(primes: &mut Vec<u64>, num: &mut u64) {
-    loop {
-        if !primes.iter().any(|prime| *num % prime == 0) {
-            primes.push(*num);
-            break;
-        }
-        *num += 1;
-    }
 }
