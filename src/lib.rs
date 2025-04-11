79f2588798cf088e5089e719b2ed3d18a5d2a32c
RezaBanivakil <rezabaniv@gmail.com>
2025-04-11 21:51:08 +0330

    0.1.0
    first implementation that pass all test but terrible performance

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let mut output = Vec::with_capacity(1);
+    let primes = calc_primes(n);
+
+    let mut n = n;
+    'outer: while n != 1 {
+        for prime in &primes {
+            if n % prime == 0 {
+                n = n / prime;
+                output.push(*prime);
+                continue 'outer;
+            }
+        }
+    }
+
+    output
+}
+
+fn calc_primes(n: u64) -> Vec<u64> {
+    let mut num = 2;
+    let mut primes = Vec::new();
+    while num < n + 1 {
+        if primes.iter().any(|prime| num % prime == 0) {
+            num += 1;
+            continue;
+        }
+        primes.push(num);
+    }
+    primes
 }
